f233d750ada8ffb379620ed694dcebd3da914185
abigael mwelu <abigael.mutuku@student.moringaschool.com>
2023-09-21 09:00:03 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -11,3 +11,14 @@ class GetRequester:
 
     def load_json(self):
         pass
+
+    def get_response_body(self):
+        pass
+        URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(URL)
+        return response.content
+
+    def load_json(self):
+        pass
+        people = json.loads(self.get_response_body())
+        return people
